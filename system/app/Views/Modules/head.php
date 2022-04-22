772685ea1dbf7a86c0a4435c9f86ef660f2dcd13
williamenrique <william21enrique@gmail.com>
2022-04-22 13:32:59 -0400

    colores listos

--- system/app/Views/Modules/head.php
+++ system/app/Views/Modules/head.php
@@ -6,8 +6,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?= $data['page_tag']?></title>
-		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;500;700&display=swap" rel="stylesheet">
-		<link href="<?= PLUGINS?>sweetalert/sweetalert2@10.css"></link>
+		<!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;500;700&display=swap" rel="stylesheet">
+		<link href="<?= PLUGINS?>sweetalert/sweetalert2@10.css"></link> -->
 		<link rel="stylesheet" href="<?= CSS?>dash.css">
 	</head>
 
